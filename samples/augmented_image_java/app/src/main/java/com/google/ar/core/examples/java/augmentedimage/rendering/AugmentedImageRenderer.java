6cb2988dd2aaff74c04ab495b096213e87e9af91
Mario Bodemann <mario@contentful.com>
2018-08-21 16:37:54 +0200

    Adding card colour renderer

--- samples/augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/rendering/AugmentedImageRenderer.java
+++ samples/augmented_image_java/app/src/main/java/com/google/ar/core/examples/java/augmentedimage/rendering/AugmentedImageRenderer.java
@@ -15,51 +15,47 @@
 package com.google.ar.core.examples.java.augmentedimage.rendering;
 
 import android.content.Context;
+
 import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.Pose;
 import com.google.ar.core.examples.java.augmentedimage.rendering.ObjectRenderer.BlendMode;
+
 import java.io.IOException;
 
-/** Renders an augmented image. */
+/**
+ * Renders an augmented image.
+ */
 public class AugmentedImageRenderer {
   private static final String TAG = "AugmentedImageRenderer";
 
   private static final float TINT_INTENSITY = 0.1f;
   private static final float TINT_ALPHA = 1.0f;
   private static final int[] TINT_COLORS_HEX = {
-    0x000000, 0xF44336, 0xE91E63, 0x9C27B0, 0x673AB7, 0x3F51B5, 0x2196F3, 0x03A9F4, 0x00BCD4,
-    0x009688, 0x4CAF50, 0x8BC34A, 0xCDDC39, 0xFFEB3B, 0xFFC107, 0xFF9800,
+      0x000000, 0xF44336, 0xE91E63, 0x9C27B0, 0x673AB7, 0x3F51B5, 0x2196F3, 0x03A9F4, 0x00BCD4,
+      0x009688, 0x4CAF50, 0x8BC34A, 0xCDDC39, 0xFFEB3B, 0xFFC107, 0xFF9800,
   };
 
-  private final ObjectRenderer imageFrameUpperLeft = new ObjectRenderer();
-  private final ObjectRenderer imageFrameUpperRight = new ObjectRenderer();
-  private final ObjectRenderer imageFrameLowerLeft = new ObjectRenderer();
-  private final ObjectRenderer imageFrameLowerRight = new ObjectRenderer();
+  private final ObjectRenderer bellRenderer = new ObjectRenderer();
+  private final ObjectRenderer heartRenderer = new ObjectRenderer();
+  private final ObjectRenderer leafRenderer = new ObjectRenderer();
+  private final ObjectRenderer acornRenderer = new ObjectRenderer();
 
-  public AugmentedImageRenderer() {}
+  public AugmentedImageRenderer() {
+  }
+
+  public void createRendererOnGlThread(Context context, ObjectRenderer renderer, String modelPath, String texturePath) throws IOException {
+    renderer.createOnGlThread(
+        context, modelPath, texturePath);
+    renderer.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+    renderer.setBlendMode(BlendMode.SourceAlpha);
+  }
 
   public void createOnGlThread(Context context) throws IOException {
-
-    imageFrameUpperLeft.createOnGlThread(
-        context, "models/frame_upper_left.obj", "models/frame_base.png");
-    imageFrameUpperLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-    imageFrameUpperLeft.setBlendMode(BlendMode.SourceAlpha);
-
-    imageFrameUpperRight.createOnGlThread(
-        context, "models/frame_upper_right.obj", "models/frame_base.png");
-    imageFrameUpperRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-    imageFrameUpperRight.setBlendMode(BlendMode.SourceAlpha);
-
-    imageFrameLowerLeft.createOnGlThread(
-        context, "models/frame_lower_left.obj", "models/frame_base.png");
-    imageFrameLowerLeft.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-    imageFrameLowerLeft.setBlendMode(BlendMode.SourceAlpha);
-
-    imageFrameLowerRight.createOnGlThread(
-        context, "models/frame_lower_right.obj", "models/frame_base.png");
-    imageFrameLowerRight.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-    imageFrameLowerRight.setBlendMode(BlendMode.SourceAlpha);
+    createRendererOnGlThread(context, bellRenderer, "models/andy.obj", "models/andy.png");
+    createRendererOnGlThread(context, heartRenderer, "models/heart.obj", "models/colors.png");
+    createRendererOnGlThread(context, leafRenderer, "models/leaf.obj", "models/colors.png");
+    createRendererOnGlThread(context, acornRenderer, "models/acorn.obj", "models/colors.png");
   }
 
   public void draw(
@@ -71,49 +67,30 @@ public class AugmentedImageRenderer {
     float[] tintColor =
         convertHexToColor(TINT_COLORS_HEX[augmentedImage.getIndex() % TINT_COLORS_HEX.length]);
 
-    Pose[] localBoundaryPoses = {
-      Pose.makeTranslation(
-          -0.5f * augmentedImage.getExtentX(),
-          0.0f,
-          -0.5f * augmentedImage.getExtentZ()), // upper left
-      Pose.makeTranslation(
-          0.5f * augmentedImage.getExtentX(),
-          0.0f,
-          -0.5f * augmentedImage.getExtentZ()), // upper right
-      Pose.makeTranslation(
-          0.5f * augmentedImage.getExtentX(),
-          0.0f,
-          0.5f * augmentedImage.getExtentZ()), // lower right
-      Pose.makeTranslation(
-          -0.5f * augmentedImage.getExtentX(),
-          0.0f,
-          0.5f * augmentedImage.getExtentZ()) // lower left
-    };
-
     Pose anchorPose = centerAnchor.getPose();
-    Pose[] worldBoundaryPoses = new Pose[4];
-    for (int i = 0; i < 4; ++i) {
-      worldBoundaryPoses[i] = anchorPose.compose(localBoundaryPoses[i]);
-    }
-
-    float scaleFactor = 1.0f;
+    float scaleFactor = 1f;
     float[] modelMatrix = new float[16];
 
-    worldBoundaryPoses[0].toMatrix(modelMatrix, 0);
-    imageFrameUpperLeft.updateModelMatrix(modelMatrix, scaleFactor);
-    imageFrameUpperLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+    anchorPose.toMatrix(modelMatrix, 0);
+    ObjectRenderer renderer = null;
+    if (augmentedImage.getName().endsWith("bell.png")) {
+      renderer = bellRenderer;
+    } else if (augmentedImage.getName().endsWith("heart.png")) {
+      renderer = heartRenderer;
+    } else if (augmentedImage.getName().endsWith("leaf.png")) {
+      renderer = leafRenderer;
+    } else if (augmentedImage.getName().endsWith("acorn.png")) {
+      renderer = acornRenderer;
+    }
 
-    worldBoundaryPoses[1].toMatrix(modelMatrix, 0);
-    imageFrameUpperRight.updateModelMatrix(modelMatrix, scaleFactor);
-    imageFrameUpperRight.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+    if (renderer != null) {
+      draw(renderer, modelMatrix, scaleFactor, viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+    }
+  }
 
-    worldBoundaryPoses[2].toMatrix(modelMatrix, 0);
-    imageFrameLowerRight.updateModelMatrix(modelMatrix, scaleFactor);
-    imageFrameLowerRight.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
-
-    worldBoundaryPoses[3].toMatrix(modelMatrix, 0);
-    imageFrameLowerLeft.updateModelMatrix(modelMatrix, scaleFactor);
-    imageFrameLowerLeft.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
+  private void draw(ObjectRenderer renderer, float[] modelMatrix, float scaleFactor, float[] viewMatrix, float[] projectionMatrix, float[] colorCorrectionRgba, float[] tintColor) {
+    renderer.updateModelMatrix(modelMatrix, scaleFactor);
+    renderer.draw(viewMatrix, projectionMatrix, colorCorrectionRgba, tintColor);
   }
 
   private static float[] convertHexToColor(int colorHex) {
@@ -121,6 +98,6 @@ public class AugmentedImageRenderer {
     float red = ((colorHex & 0xFF0000) >> 16) / 255.0f * TINT_INTENSITY;
     float green = ((colorHex & 0x00FF00) >> 8) / 255.0f * TINT_INTENSITY;
     float blue = (colorHex & 0x0000FF) / 255.0f * TINT_INTENSITY;
-    return new float[] {red, green, blue, TINT_ALPHA};
+    return new float[]{red, green, blue, TINT_ALPHA};
   }
 }
